a9df7757e1629b5b99eaf1b7e273ca0285c9c4c4
root <root@localhost>
2014-12-20 23:46:00 -0500

    Stripped down vm.h

--- vm.h
+++ vm.h
@@ -10,21 +10,12 @@
 #ifndef MTL_ARC_VM_H
 #define MTL_ARC_VM_H
 
-#include <stdarg.h>
+#include "port.h"
 
 typedef struct vm vm_t;
-typedef struct cell cell_t;
 
-//==============================================================
-// Port System
-//==============================================================
-
-typedef enum {
-	PORT_STREAM,
-	PORT_STRING,
-	PORT_SOCKET,
-	NUM_PORT_TYPES
-} port_type_t;
+int vm_printf(vm_t *, const char *fmt, ...);
+int vm_errorf(vm_t *, const char *fmt, ...);
 
 typedef enum {
 	VM_IN_PORT,
@@ -32,52 +23,6 @@ typedef enum {
 	VM_ERR_PORT,
 	VM_DBG_PORT
 } vm_port_t;
-
-typedef const char * (*port_info_func_t)(vm_t *, cell_t *, cell_t **);
-typedef int (*port_open_func_t)(vm_t *, cell_t *, const char *mode, cell_t *params);
-typedef int (*port_close_func_t)(vm_t *, cell_t *);
-typedef int (*port_getc_func_t)(vm_t *, cell_t *);
-typedef long (*port_size_func_t)(vm_t *, cell_t *);
-typedef size_t (*port_read_func_t)(vm_t *, cell_t *, size_t, char *);
-typedef int (*port_vprintf_func_t)(vm_t *, cell_t *, const char *fmt, va_list);
-typedef cell_t *(*port_owns_func_t)(secd_t*, cell_t *, cell_t **, cell_t **, cell_t **);
-typedef cell_t *(*vm_port_func_t)(secd_t*, vm_port_t);
-
-typedef struct {
-	port_info_func_t info;
-	port_open_func_t open;
-	port_close_func_t close;
-	port_getc_func_t getc;
-	port_size_func_t size;
-	port_read_func_t read;
-	port_vprintf_func_t vprintf;
-	port_owns_func_t owns;
-	vm_port_func_t vm_port;
-} port_ops_t;
-
-typedef struct {
-	port_type_t type;
-	int input  : 1; // allow input?
-	int output : 1; // allow output?
-	long data[2];
-} port_t;
-
-cell_t *vm_stdin(vm_t *);
-cell_t *vm_stdout(vm_t *);
-cell_t *vm_stderr(vm_t *);
-cell_t *vm_stddbg(vm_t *);
-cell_t *vm_set_port(vm_t *, std_port_t, cell_t *);
-
-int vm_open_port(vm_t *, cell_t *, const char *mode, cell_t *info);
-long vm_port_size(vm_t *, cell_t *);
-int vm_close_port(vm_t *, cell_t *);
-
-int vm_getc(vm_t *, cell_t *);
-size_t vm_read(vm_t *, cell_t *, size_t, char *);
-
-//==============================================================
-// Virtual Machine
-//==============================================================
 
 struct vm {
 	cell_t *begin;
@@ -93,7 +38,7 @@ struct vm {
 	cell_t *output_port;
 	cell_t *error_port;
 	cell_t *debug_port;
-	port_ops_t port_ops[NUM_PORT_TYPES];
+	unsigned long tick;
 };
 
 #endif /* MTL_ARC_VM_H */
