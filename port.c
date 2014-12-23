37e8d6ab2dc2d8b00620f78a35e08619770fdd28
root <root@localhost>
2014-12-23 16:00:24 -0500

    Initial commit of port.c

---
+++ port.c
@@ -0,0 +1,79 @@
+//==============================================================
+// MTL-Arc
+//   A new implementation of the Arc language
+// Copyright (C) 2014 Camden Smallwood
+//==============================================================
+// port.c
+//   Port system - I/O to files, strings and sockets
+//==============================================================
+
+#include <stdlib.h>
+#include "port.h"
+
+int port_readb(port_t *port) {
+	unsigned char result;
+	
+	if (port_state(port) != PORT_OPEN)
+		return EOF;
+
+	switch (port_type(port)) {
+		case PORT_FILE:
+		case PORT_SOCKET:
+			read(port_fd(port), &result, sizeof(result));
+			break;
+		default:
+			if (port_pos(port) >= port_size(port))
+				return EOF;
+			result = port_data(port)[port_pos(port)];
+			(*port).pos++;
+			break;
+	}
+	
+	return result;
+}
+
+int port_readc(port_t *port) {
+	char result;
+	
+	if (port_state(port) != PORT_OPEN)
+		return EOF;
+
+	switch (port_type(port)) {
+		case PORT_FILE:
+		case PORT_SOCKET:
+			read(port_fd(port), &result, sizeof(result));
+			break;
+		default:
+			if (port_pos(port) >= port_size(port))
+				return EOF;
+			result = port_data(port)[port_pos(port)];
+			(*port).pos++;
+			break;
+	}
+	
+	return result;
+}
+
+int port_peekc(port_t *port) {
+	char result;
+	FILE *file;
+	
+	if (port_state(port) != PORT_OPEN)
+		return EOF;
+	
+	switch (port_type(port)) {
+		case PORT_FILE:
+		case PORT_SOCKET:
+			file = fdopen(port_fd(port), "r");
+			result = fgetc(file);
+			ungetc(result, file);
+			break;
+		default:
+			if (port_pos(port) >= port_size(port))
+				return EOF;
+			result = port_data(port)[port_pos(port) + 1];
+			break;
+	}
+	
+	return result;
+}
