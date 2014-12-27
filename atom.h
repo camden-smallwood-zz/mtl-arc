a01d06af5b8e56f87af6d410061be26b28517bd3
root <root@localhost>
2014-12-27 14:22:56 -0500

    Updated atom.h

--- atom.h
+++ atom.h
@@ -1,3 +1,11 @@
+//==============================================================
+// MTL-Arc
+//   A new implementation of the Arc language
+// Copyright (C) 2014 Camden Smallwood
+//==============================================================
+// atom.h
+//   Main data structure
+//==============================================================
 
 #ifndef MTL_ARC_ATOM_H
 #define MTL_ARC_ATOM_H
@@ -52,52 +60,63 @@ struct atom {
 	};
 };
 
+extern atom_t *nil;
+#define no(atom) ((atom) == nil)
+
 #define integer(atom) ((atom)->integer)
+atom_t *new_integer(const long long value);
+atom_t *coerce_integer(atom_t *value);
+
 #define decimal(atom) ((atom)->decimal)
+atom_t *new_decimal(const double value);
+atom_t *coerce_decimal(atom_t *value);
+
 #define character(atom) ((atom)->character)
+atom_t *new_character(const char value);
+atom_t *coerce_character(atom_t *value);
+
 #define string(atom) ((atom)->string)
+atom_t *new_string(const char *value);
+atom_t *coerce_string(atom_t *value);
+
+extern atom_t *symbol_table;
 #define symbol(atom) ((atom)->symbol)
-#define cons(car, cdr) new_cons((car), (cdr))
+atom_t *new_symbol(const char *value);
+atom_t *coerce_symbol(atom_t *value);
+atom_t *intern(const char *symbol);
+
 #define car(atom) ((atom)->car)
 #define cdr(atom) ((atom)->cdr)
+atom_t *new_cons(atom_t *car, atom_t *cdr);
+atom_t *coerce_cons(atom_t *value);
+#define cons(car, cdr) new_cons((car), (cdr))
+
 #define args(atom) ((atom)->args)
 #define body(atom) ((atom)->body)
 #define env(atom) ((atom)->env)
-#define tag(atom) ((atom)->tag)
-#define rep(atom) ((atom)->rep)
-#define table(atom) ((atom)->table)
-#define port(atom) ((atom)->port)
-#define help(atom) ((atom)->help)
-#define func(atom) ((atom)->func)
-#define context(atom) ((atom)->context)
-#define message(atom) ((atom)->message)
-#define continuation(atom) ((atom)->continuation)
-
-atom_t *new_atom(atom_type_t type);
-atom_t *new_integer(const long long value);
-atom_t *new_decimal(const double value);
-atom_t *new_character(const char value);
-atom_t *new_string(const char *value);
-atom_t *new_symbol(const char *value);
-atom_t *new_cons(atom_t *car, atom_t *cdr);
 atom_t *new_lambda(atom_t *args, atom_t *body, atom_t *env);
 atom_t *new_macro(atom_t *args, atom_t *body, atom_t *env);
+
+#define tag(atom) ((atom)->tag)
+#define rep(atom) ((atom)->rep)
 atom_t *new_tagged(atom_t *tag, atom_t *rep);
+
+#define table(atom) ((atom)->table)
 atom_t *new_table(atom_t *entries);
+atom_t *coerce_table(atom_t *value);
+
+#define port(atom) ((atom)->port)
 atom_t *new_input(port_t *port);
 atom_t *new_output(port_t *port);
 atom_t *new_socket(port_t *port);
+
+#define help(atom) ((atom)->help)
+#define func(atom) ((atom)->func)
 atom_t *new_builtin(const char *help, atom_t *(*func)(atom_t *));
+
+#define context(atom) ((atom)->context)
+#define message(atom) ((atom)->message)
+#define continuation(atom) ((atom)->continuation)
 atom_t *new_exception(atom_t *context, const char *message, atom_t *continuation);
-
-atom_t *intern(const char *symbol);
-
-atom_t *coerce_integer(atom_t *value);
-atom_t *coerce_decimal(atom_t *value);
-atom_t *coerce_character(atom_t *value);
-atom_t *coerce_string(atom_t *value);
-atom_t *coerce_symbol(atom_t *value);
-atom_t *coerce_cons(atom_t *value);
-atom_t *coerce_table(atom_t *value);
 
 #endif /* MTL_ARC_ATOM_H */
